320f94bbfcb616c2d7662efb0c108f71bfad47f6
leoato <leoatoapple@gmail.com>
2026-01-25 02:25:10 +0900

    Delete db_config.js

--- db_config.js
+++
@@ -1,4 +0,0 @@
-const SB_URL = "https://onoowabyrjpkkuwauhmn.supabase.co";
-const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ub293YWJ5cmpwa2t1d2F1aG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyNDcxMDEsImV4cCI6MjA4NDgyMzEwMX0.-BW364OB0IVmBBvePNYWgp6a-JwI7D1mVGa8aeTJwOA";
-const supabase = supabase.createClient(SB_URL, SB_KEY);
-const USER_ID = "dongkyun_pt"; // 사용자 식별값
